aa16073f84960d73ec5855ab0681240329172b49
whynot <anil@panix.com>
2021-09-08 14:01:42 -0400

    Added Test

--- code/Livecode/src/test/java/ttl/larku/live/FirstShot.java
+++ code/Livecode/src/test/java/ttl/larku/live/FirstShot.java
@@ -2,15 +2,31 @@ package ttl.larku.live;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author whynot
  */
+
+class ClassToTest
+{
+    private int i;
+    public ClassToTest(int i) {
+        this.i =i;
+    }
+
+    public int getI() {
+        return i;
+    }
+}
+
 public class FirstShot {
 
     @Test
     public void firstGuy() {
-        System.out.println("Here we go");
+        ClassToTest ti = new ClassToTest(10);
 
+        assertEquals(10, ti.getI());
     }
 
 }
