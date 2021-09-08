21707e6f4467bbce7e4e3907b45f2d5f44000966
whynot <anil@panix.com>
2021-09-08 07:51:59 -0400

    Added pom.xml

---
+++ code/Livecode/src/test/java/ttl/larku/live/FirstShot.java
@@ -0,0 +1,15 @@
+package ttl.larku.live;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author whynot
+ */
+public class FirstShot {
+
+    @Test
+    public void firstGuy() {
+        System.out.println("Here we go");
+    }
+
+}
