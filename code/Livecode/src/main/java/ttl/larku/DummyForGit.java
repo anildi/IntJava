dbe4db79e09bfd4475b3f7e79a630a4c6c73ed2e
whynot <anil@panix.com>
2021-09-08 13:18:54 -0400

    Added src directory

---
+++ code/Livecode/src/main/java/ttl/larku/DummyForGit.java
@@ -0,0 +1,11 @@
+package ttl.larku;
+
+/**
+ * @author whynot
+ */
+public class DummyForGit {
+
+    public static void main(String[] args) {
+        System.out.println("Nothing here, just a dummy for git");
+    }
+}
